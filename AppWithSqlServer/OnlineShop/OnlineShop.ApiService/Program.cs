08f3632722140d7786052a355a912e68bb03dfd0
fiodarsazanavets <f.sazanavets@gmail.com>
2025-12-21 14:27:05 +0000

    Add a placeholder for SQL Server integration example

---
+++ AppWithSqlServer/OnlineShop/OnlineShop.ApiService/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using OnlineShop.ApiService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient(
+    "OidcBackchannel", o => o.BaseAddress = new("http://idp"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+
+})
+.AddJwtBearer()
+.ConfigureApiJwt();
+
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+app.MapGet("/", () => "API service is running. Navigate to /weatherforecast to see sample data.");
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapDefaultEndpoints();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ AppWithSqlServer/OnlineShop/OnlineShop.AppHost/AppHost.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Hosting;
+using OnlineShop.AppHost.Extensions;
+using OnlineShop.MailDev.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var maildev = builder.AddMailDev("maildev");
+
+var idp = builder.AddKeycloakContainer(
+    "idp", tag: "23.0")
+    .ImportRealms("Keycloak")
+    .WithExternalHttpEndpoints();
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.OnlineShop_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health")
+    .WithReference(idp)
+    .WaitFor(idp);
+    
+
+var webFrontend = builder
+    .AddProject<Projects.OnlineShop_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WithReference(idp, env: "Identity__ClientSecret")
+    .WaitFor(idp)
+    .WithReference(cache)
+    .WaitFor(cache);
+
+if (builder.Environment.IsDevelopment())
+{
+    var webAppHttp = webFrontend.GetEndpoint("http");
+    var webAppHttps = webFrontend.GetEndpoint("https");
+
+    idp.WithEnvironment("WEBAPP_HTTP", () =>
+        $"{webAppHttp.Scheme}://{webAppHttp.Host}:{webAppHttp.Port}");
+
+    if (webAppHttps.Exists)
+    {
+        idp.WithEnvironment("WEBAPP_HTTP_CONTAINERHOST",
+            webAppHttps);
+        idp.WithEnvironment("WEBAPP_HTTPS", () =>
+            $"{webAppHttps.Scheme}://{webAppHttps.Host}:{webAppHttps.Port}");
+    }
+    else
+    {
+        idp.WithEnvironment("WEBAPP_HTTP_CONTAINERHOST",
+            webAppHttp);
+    }
+}
+
+
+builder.Build().Run();
